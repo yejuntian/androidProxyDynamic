7f7b90925ba3acf77df653878d444c6d9ae60f96
yejuntian <1961993790@qq.com>
2018-01-20 15:43:00 +0800

    [Feature]:搭建项目

---
+++ app/src/main/java/proxy/own/com/androidproxydynamic/MainActivity.java
@@ -0,0 +1,13 @@
+package proxy.own.com.androidproxydynamic;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
